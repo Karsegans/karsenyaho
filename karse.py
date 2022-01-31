bb82771c3aa59b55490f032f1e68951464fd7f7a
Karsegans <98702696+Karsegans@users.noreply.github.com>
2022-02-01 02:11:44 +0700

    Add files via upload

---
+++ karse.py
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+     <title> karse gansss </title>
+</head>
+<body>
+<p>hello word</p>
+<p>selamat datang di webisite saya</p>
+<hr>
+<p><b>dibuat oleh karse</b></p>
+<h1>hello world</h1>
+<h4>hello guys</4>
+<ol>
+   <li>hello guys</li>
+   <li>kembali lagi dengan gw karse</li>
+   <li>kali ini gw mau gabut</li>
+</ol>
+<ul>
+   <li>BAKA</li>
+   <li>baka</li>
+   <li>baka</li>
+</ul>
+<ul>
+ <li><a href="https://instagram.com/tfqrmn01?utm_medium=copy_link"> Instagram</a></li>
+
+ <li><a href="https://instagram.com/a.zlianti?utm_medium=copy_link">instagram</a></li>
+</ul>
+
+
+<hr>
+<br>
+
+<p>pencet<a href="content://com.coloros.filemanager/root/storage/emulated/0/hello.html"> disini</a> untuk kesebelah</p>
+
+
+</body>
+</html>

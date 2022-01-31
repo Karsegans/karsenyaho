f504d96701bb0ebeffe015504dae154888f79d4b
Karsegans <98702696+Karsegans@users.noreply.github.com>
2022-01-31 15:02:31 +0700

    Add files via upload

---
+++ karse.py
@@ -0,0 +1,8 @@
+puclic class xmm1{
+public static Void main(String[]argas){
+System.out.print("selamat siang kelas...");
+System.out.print("nama:Taufiqurachman...");
+System.out.print("kelas:xmm1...");
+System.out.print("jurusan:multimedia...");
+}
+}
